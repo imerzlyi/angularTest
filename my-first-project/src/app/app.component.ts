8426c5560652eeceb8ff882ad6ad4b2c5ea3f54c
imerzlyi <58734387+imerzlyi@users.noreply.github.com>
2019-12-10 18:44:27 +0200

    lab2

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+  
+  timerEmitted(event) {
+    if (event % 2 != 0) {
+      let odd = new OddComponent();
+      console.log("odd created");
+    }
+    else {
+      let even = new EvenComponent();
+      console.log("even created");
+    }
+  }
 }
